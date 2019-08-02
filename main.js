544fa701a1a364524cd5abfd2907c4d8562127ca
JonhLucas <38635930+JonhLucas@users.noreply.github.com>
2019-08-01 23:59:18 -0300

    visual
    copia de imagens, não move

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-function dragStart(ev)
+function dragStart2(ev)
 {
     ev.dataTransfer.setData("ID", ev.target.getAttribute('id'));
+}
+function dragStart(ev)
+{
+    var node = document.getElementById(ev.target.getAttribute('id')).cloneNode(true);
+    node.id = node.id + contador;
+    contador = contador + 1;
+    ev.dataTransfer.setData("ID",  node.id);
+    ev.target.appendChild(node);
+    //console.log(node.ondragstart); 
+    node.ondragstart = function ondragstart(event){ return dragStart2(event);};
+    //console.log(node.ondragstart);  
 }
 function dragOver(ev)
 {
@@ -10,7 +21,7 @@ function dragDrop(ev)
 {
     ev.preventDefault();
     var produtoSelecionado = ev.dataTransfer.getData("ID");
-    console.log(ev.target);
+    console.log(produtoSelecionado);
     if(ev.target.tagName == "IMG")
     {
         ev.target.parentNode.appendChild(document.getElementById(produtoSelecionado));
@@ -20,3 +31,4 @@ function dragDrop(ev)
         ev.target.appendChild(document.getElementById(produtoSelecionado));
     }
 }
+var contador = 0;
