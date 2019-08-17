c59edba5c7300a9502573e43960ac97605e0aed6
JonhLucas <38635930+JonhLucas@users.noreply.github.com>
2019-08-16 21:36:51 -0300

    Update sortable.js

--- sortable.js
+++ sortable.js
@@ -1,4 +1,16 @@
-$(document).ready(function()
-      {
-        $("#carrinho").sortable();
-      });
+$( function() {
+  $( "#executor" ).sortable({
+  revert: true
+  });
+  $( ".instrução" ).draggable({
+  connectToSortable: "#executor",
+  helper: "clone",
+  revert: "invalid"
+  });
+  $( "ul, li" ).disableSelection();
+  $('#campode_exclusao').droppable({
+      drop: function(event, ui) {
+          $(ui.draggable).remove();
+      }
+  });
+});
